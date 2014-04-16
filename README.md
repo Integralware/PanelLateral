4c98753c6f85ab2047fda66293551113fb0a2c2c
Javier Martín <javier.martin@integralware.es>
2014-04-16 20:14:45 +0200

    Vamos a cambiar
    primeros cambios del proyecto

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ PanelLateral
 ============
 
 Panel lateral
+
+Proyecto de ejemplo para estudiar el uso de GitHUB
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    alert("Esto cambiará!");
     /*
      * VARIABLES GLOBALES
     */
